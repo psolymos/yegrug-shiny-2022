8964d7c39b15acd5c16dcce494206a905ccb2635
Peter Solymos <psolymos@gmail.com>
2022-11-20 20:18:55 -0700

    Deployment
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- 04-shiny.R
+++ 04-shiny.R
@@ -1,3 +1,16 @@
 #' Run the app:
 shiny::runApp("app")
 #' Now go and check the files in the ./app folder
+
+#' Deploy
+
+## https://psolymos.shinyapps.io/BananaAI/
+
+# rsconnect::setAccountInfo(name = "psolymos",
+#   token = "xxxxxx",
+#   secret = "xxxxxx")
+
+rsconnect::deployApp("app", 
+  appName = "BananaAI",
+  account = "psolymos",
+  forceUpdate = TRUE)
